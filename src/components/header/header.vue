9ee8b193d02c5716341ee31f49378483c65b7f81
litbear <litbear@foxmail.com>
2017-03-25 10:51:06 +0800

    lesson 5

---
+++ src/components/header/header.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="header">
+    我是header
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  /* eslint-disable no-undef */
+  export default {
+    data () {
+      return {};
+    },
+    components: {},
+    methods: {}
+  };
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
---
+++ src/components/seller/seller.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>I am seller</div>
+</template>
+
+<script type="text/ecmascript-6">
+  /* eslint-disable no-undef */
+  export default {
+    data () {
+      return {};
+    },
+    components: {},
+    methods: {}
+  };
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
